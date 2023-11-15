b20f77d0345594f30052a3beedf5d2ca4d1ac383
soroush-paidar <84630919+soroush-paidar@users.noreply.github.com>
2023-11-15 18:31:27 +0000

    should be able to repeatedly offer responses to infinite questions.

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ openai.api_key = OPENAI_API_KEY
 # Initialize the recognizer
 recognizer = sr.Recognizer()
 # Ask for speech input
-print(What's up?)
+print('Hello, I am DONNA, your personal secretary. What do you need?')
 if method = 1:
   with sr.Microphone() as source:
-    print("Speak something:")
+    print("Say something:")
     audio = recognizer.listen(source)
     input_text = recognizer.recognize_google(audio)
 if method = 2:
-  input_text = input()
+  input_text = input("What's on your mind?":)
   messages=[{"role": "user", "content": a}]
 
 response = openai.ChatCompletion.create(
@@ -39,3 +39,15 @@ os.system("mpg123 response.mp3")
 yon = input()
 if yon == 'yes':
   os.remove("response.mp3")
+
+while True:
+  print('Anything else?')
+  input_text = input('Enter your response:')
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo")
+
+  print(response)
+  tts = gTTS(text=response['choices'][0]['text'], lang='en')
+  tts.save("response.mp3")
+  os.system("mpg123 response.mp3")
+  
