272f7bd48d636a2680a685b2107e02a1a664791b
soroush-paidar <84630919+soroush-paidar@users.noreply.github.com>
2023-11-15 15:50:44 +0000

    i think it can take speech, the whole things a fucking mess though i already know theres like 25 syntax errors

--- main.py
+++ main.py
@@ -4,16 +4,27 @@
 ## pip install openai
 ## pip install gTTS
 
-from typing import Mapping
-import openai
-from gtts import gTTS
 import os
+from typing import Mapping
+
+import openai
+import speech_recognition as sr
+from gtts import gTTS
 
 OPENAI_API_KEY = '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'
 openai.api_key = OPENAI_API_KEY
-
-a = input()
-messages=[{"role": "user", "content": a}]
+# Initialize the recognizer
+recognizer = sr.Recognizer()
+# Ask for speech input
+print(What's up?)
+if method = 1:
+  with sr.Microphone() as source:
+    print("Speak something:")
+    audio = recognizer.listen(source)
+    input_text = recognizer.recognize_google(audio)
+if method = 2:
+  input_text = input()
+  messages=[{"role": "user", "content": a}]
 
 response = openai.ChatCompletion.create(
 model="gpt-3.5-turbo")
@@ -28,4 +39,3 @@ os.system("mpg123 response.mp3")
 yon = input()
 if yon == 'yes':
   os.remove("response.mp3")
-
