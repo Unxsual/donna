9856ac9537ffbe0ef15d24d641305109b15cef39
sorou <soroush.paidar@gmail.com>
2024-01-09 21:42:26 -0500

    Threw everything out of the window and started from scratch. Shit works now.

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
 import os
-os.system("pip install --upgrade pip")
-os.system("pip install openai")
-os.system("pip install speechrecognition")
-os.system("pip install openai==0.28")
+upgradeq = input("Would you like to check pip and openai versions? If yes, input 'Y'. If not, input 'N'.")
+if upgradeq.strip() == "Y":
+  os.system("pip install --upgrade pip")
+  os.system("pip install openai")
+  os.system("pip install openai==0.28")
+else:
+  print("loading program")
 import openai
-from typing import Mapping
-from gtts import gTTS
-import speech_recognition as sr
 
-openai.api_key = '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'
+openai.api_key = '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'
 
-recognizer = sr.Recognizer()
-# Ask for speech input
-print("Hello, I am your personal secretary! What's up?")
-mth = input()
-if mth == 1:
-  with sr.Microphone() as source:
-    print("Say something:")
-    audio = recognizer.listen(source)
-    input_text = recognizer.recognize_google(audio)
-if mth == 2:
-  input_text = input("What's on your mind?")
-  messages=[{"role": "user", "content": a}]
+model_id = 'gpt-3.5-turbo'
 
-response = openai.ChatCompletion.create(
-model="gpt-3.5-turbo")
+def ChatGPT_conversation(conversation):
+  response = openai.ChatCompletion.create(
+    model=model_id,
+    messages=conversation
+  )
 
-print(response)
+  conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
+  return conversation
 
-# Generating audio file for audio response
-
-tts = gTTS(text=response['choices'][0]['text'], lang='en')
-tts.save("response.mp3")
-os.system("mpg123 response.mp3")
-yon = input()
-if yon == 'yes':
-  os.remove("response.mp3")
+conversation = []
+conversation.append({'role': 'system', 'content': "Say nothing."})
+conversation = ChatGPT_conversation(conversation)
+response = ('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+initial = (response.replace('assistant: ', ''))
+print(initial)
 
 while True:
-  print('Anything else?')
-  input_text = input('Enter your response:')
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo")
-
-  print(response)
-  tts = gTTS(text=response['choices'][0]['text'], lang='en')
-  tts.save("response.mp3")
-  os.system("mpg123 response.mp3")
+  prompt = input("You: ")
+  if prompt.strip() == "Print conversation.":
+    print(conversation)
+  else:
+    conversation.append({'role': 'user', 'content': prompt})
+    conversation = ChatGPT_conversation(conversation)
+    response = ('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+    final = (response.replace('assistant: ', ''))
+    print(final)
